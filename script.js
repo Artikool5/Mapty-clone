7b22b22a887a95f5def2b2f09b7eca1e70e53232
artikool <artikool5@yandex.ru>
2022-10-05 20:23:07 +0300

    Added Leaflet API

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -11,5 +10,23 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition((position)=>{alert(position)},
-    ()=>{alert("Соединение не удалось")})
+function onMapClick(e) {
+  console.log(e);
+  L.marker(e.latlng).addTo(map)
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition((position) => {
+        const {latitude, longitude} = position.coords;
+        const map = L.map('map').setView([latitude, longitude], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        map.on('click', onMapClick);
+      },
+      (positionError) => {
+        console.log(positionError.message);
+      })
+}
+
